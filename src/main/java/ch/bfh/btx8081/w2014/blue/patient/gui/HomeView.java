21ebf65fdab2aa036b89474e4eda0badfe6253ce
ghodm1 <ghodm1@bfh.ch>
2014-12-03 13:38:28 +0100

    Modified. From this page , can you navigate to Therapy,Medication and login pages with clicking on the Buttons.

---
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/HomeView.java
@@ -0,0 +1,166 @@
+/**
+ * 
+ */
+package ch.bfh.btx8081.w2014.blue.patient.gui;
+import ch.bfh.btx8081.w2014.blue.patient.controller.ControllerUI;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+
+/**Creates a new <code>HomeView</code> instance,
+ * that contains three buttons in order to see mediations,
+ * Therapy and to log out.
+ * 
+ * @author Amin
+ *
+ */
+@SuppressWarnings("serial")
+public class HomeView extends VerticalLayout implements View  {
+	
+
+	/**
+	 * 
+	 */
+	final VerticalLayout layout;
+	private AbsoluteLayout mainLayout;
+	private VerticalLayout verticalLayout;
+	private Button medicationButton;
+	private Button therapyButton;
+	private Button logoutButton;
+	
+	private PatientMainDesign design;
+	
+	/**
+	 * construct the Home view with two buttons.
+	 */
+	public HomeView(){
+		design = new PatientMainDesign(this);
+		layout=design.getLayout();
+		buildVerticalLayout_1();
+		mainLayout = design.getMainLayout();
+		createLayout();
+		addActionListnerToButtons();
+		
+		
+	}
+
+	
+	
+	
+	
+	/**
+	 * Add Listeners to Buttons
+	 */
+	private void addActionListnerToButtons(){
+		medicationButton.addClickListener(new Button.ClickListener() {
+			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				Notification.show("Go to medication page");
+				ControllerUI.navigateTo(ControllerUI.MEDICATION);
+				
+			}
+		});
+		
+		therapyButton.addClickListener(new Button .ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				//Notification.show("Go to Therapy page");
+				ControllerUI.navigateTo(ControllerUI.THERAPYVIEW);
+				
+				
+			}
+		});
+		
+		logoutButton.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				ControllerUI.navigateTo(ControllerUI.LOGINVIEW);
+				
+			}
+		});
+	}
+	
+	/**
+	 * create a layout and add this new layout at the mainLayout. the title
+	 * of this layout will be defined.
+	 */
+
+	private void createLayout() {
+
+		design.setTitleLabel("MHC-Patient");
+		verticalLayout = buildVerticalLayout_1();
+		mainLayout.addComponent(verticalLayout, "top:60.0px;left:30.0px;");
+
+	}
+	
+	
+	/**
+	 * Builds the verticallayout and defines the size. 
+	 */
+	private VerticalLayout buildVerticalLayout_1() {
+		// common part: create layout
+		verticalLayout = new VerticalLayout();
+		verticalLayout.setImmediate(false);
+		layout.setImmediate(false);
+		layout.setWidth("260px");
+		layout.setHeight("360px");
+		layout.setMargin(false);
+		verticalLayout.setWidth("260px");
+		verticalLayout.setHeight("360px");
+		verticalLayout.setMargin(false);
+		
+		
+		medicationButton=new Button("Medication");
+		verticalLayout.addComponent(medicationButton);
+		verticalLayout.setComponentAlignment(medicationButton,
+				Alignment.TOP_CENTER);
+		
+
+		therapyButton=new Button("Therapy");
+		verticalLayout.addComponent(therapyButton);
+		verticalLayout.setComponentAlignment(therapyButton,
+				Alignment.MIDDLE_CENTER);
+		
+		logoutButton=new Button("Logout");
+		verticalLayout.addComponent(logoutButton);
+		verticalLayout.setComponentAlignment(logoutButton
+				, Alignment.BOTTOM_CENTER);
+		
+		return verticalLayout;
+	}
+	
+
+
+	
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	
+	
+	
+
+	
+	
+
+}
