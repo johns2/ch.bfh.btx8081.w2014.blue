5ce82ebc552c1810109a28ed72c6f53375767b28
mahep1 <mahep1@bfh.ch>
2014-12-17 13:33:36 +0100

    Therapy modified

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Therapy.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Therapy.java
@@ -5,6 +5,8 @@ package ch.bfh.btx8081.w2014.blue.patient.gui;
 
 
 import ch.bfh.btx8081.w2014.blue.patient.controller.ControllerUI;
+import ch.bfh.btx8081.w2014.blue.patient.database.XmlFileReader;
+import ch.bfh.btx8081.w2014.blue.patient.model.showTherapy;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -13,6 +15,7 @@ import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 
@@ -115,9 +118,22 @@ public class Therapy extends VerticalLayout implements View{
 
 	@Override
 	public void enter(ViewChangeEvent event) {
+		therapyList.removeAllItems();
+		therapyList.addItems("Psychotherapy",
+				"Group psychotherapy",
+				"Cognitive behavioral therapy",
+				"Body-oriented psychotherapy",
+				"Pharmacotherapy");
+		 
+		//for(showTherapy s: XmlFileReader.getTherapies())
+		   // {
+			
+			//therapyList.addItem(new Label(s.getName()));
+		// }
+
+		
 	    
 	}
     
     
-
-}
+	}
