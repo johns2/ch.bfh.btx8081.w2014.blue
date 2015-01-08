f4047417177438e5054254145b2a88d55ef2e46d
marts1 <sabine.martin@students.bfh.ch>
2015-01-08 21:26:01 +0100

    medication gui (combobox, table)

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Medication.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Medication.java
@@ -5,17 +5,22 @@ package ch.bfh.btx8081.w2014.blue.patient.gui;
 
 import ch.bfh.btx8081.w2014.blue.patient.controller.ControllerUI;
 
+import com.vaadin.data.Item;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 
 /**
- * @author Pira
  * @author Sabine
  *
  *
@@ -26,32 +31,46 @@ public class Medication extends VerticalLayout implements View {
 	final VerticalLayout layout;
 	private AbsoluteLayout mainLayout;
 	private VerticalLayout verticalLayout;
-	private Button dosageButton;
-	private Button MedInfoButton;
+	private ComboBox calendarList;
+	private Button showInfoButton;
 	private Button homeButton;
 	private PatientMainDesign design;
+	private Table medication;
 
+	
+	/**
+	 * Constructs a MEDICATIONVIEW on the base of different parameters.
+	 */
+	
 	public Medication()
 	{
-		buildVerticalLayout_1();
+		//buildVerticalLayout_1();
 		design = new PatientMainDesign(this);
 		layout = design.getLayout();
 		mainLayout = design.getMainLayout();
 		createLayout();
 	}
 	
+	
+	/**
+	 * create a layout and add this new layout at the mainLayout. the title
+	 * of this layout will be defined.
+	 */
+	
 	private void createLayout() {
 		design.setTitleLabel("Medication");
+		//medication=new Table ("medication");
 		verticalLayout = buildVerticalLayout_1();
 		mainLayout.addComponent(verticalLayout, "top:60.0px;left:30.0px;");
+		
+		
+
 	}
+
 	
-	public void enter(ViewChangeEvent event) {
-		/*dosageButton.addClickListener(null);
-		MedInfoButton.addClickListener(null);
-		homeButton.addClickListener(null);
-		*/
-	}
+	/**
+	 * Builds the verticallayout and defines the size.
+	 */
 	
 	private VerticalLayout buildVerticalLayout_1(){
 		verticalLayout = new VerticalLayout();
@@ -60,21 +79,56 @@ public class Medication extends VerticalLayout implements View {
 		verticalLayout.setHeight("360px");
 		verticalLayout.setMargin(false);
 		
-		dosageButton = new Button("Current Dosage");
-		dosageButton.setIcon(FontAwesome.HOSPITAL_O);
-		dosageButton.setImmediate(true);
-		dosageButton.setWidth("80%");
-		dosageButton.setHeight("-1px");
-		verticalLayout.addComponent(dosageButton);
-		verticalLayout.setComponentAlignment(dosageButton, Alignment.MIDDLE_CENTER);
 		
-		MedInfoButton = new Button("Medicine Information");
-		MedInfoButton.setIcon(FontAwesome.INFO_CIRCLE);
-		MedInfoButton.setImmediate(true);
-		MedInfoButton.setWidth("80%");
-		MedInfoButton.setHeight("-1px");
-		verticalLayout.addComponent(MedInfoButton);
-		verticalLayout.setComponentAlignment(MedInfoButton, Alignment.MIDDLE_CENTER);
+		/**
+		 * ComboBox for a list of days
+		 */
+
+		calendarList = new ComboBox();
+		calendarList.setInputPrompt("Select a Day");
+		calendarList.setHeight("-1px");
+		calendarList.setNewItemsAllowed(true);
+		calendarList.setImmediate(true);
+		calendarList.setNullSelectionAllowed(false);
+		verticalLayout.addComponent(calendarList);
+		verticalLayout.setComponentAlignment(calendarList, Alignment.TOP_CENTER);
+		
+		
+		/**
+		 * Button to confirm selected day
+		 */
+		
+		showInfoButton = new Button("Show Medication");
+		showInfoButton.setIcon(FontAwesome.INFO_CIRCLE);
+		showInfoButton.setImmediate(true);
+		showInfoButton.setWidth("80%");
+		showInfoButton.setHeight("-1px");
+		verticalLayout.addComponent(showInfoButton);
+		verticalLayout.setComponentAlignment(showInfoButton, Alignment.TOP_CENTER);
+		
+		
+		/**
+		 * * Table for a list of medication for selected day
+		 */
+		
+		medication=new Table ();
+		medication.setWidth("100%");
+		medication.setHeight("100%");
+		medication.addContainerProperty("Medication", String.class, null);
+		medication.addContainerProperty("Dosage", String.class,  null);
+		medication.addContainerProperty("Time", String.class, null);
+		Object cipralex = medication.addItem();
+		Item row1 = medication.getItem(cipralex);
+		row1.getItemProperty("Medication").setValue("Cipralex");
+		verticalLayout.addComponent(medication);
+		verticalLayout.setComponentAlignment(medication, Alignment.TOP_CENTER);
+		//mainLayout.addComponent(medication);
+		//mainLayout.addComponent(verticalLayout, "top:60.0px;left:30.0px;");
+		
+		
+		/**
+		 * Button to get back to the HOMEVIEW
+		 */
 		
 		homeButton = new Button("Home");
 		homeButton.setIcon(FontAwesome.HOME);
@@ -83,6 +137,11 @@ public class Medication extends VerticalLayout implements View {
 		homeButton.setHeight("-1px");
 		homeButton.addClickListener(new Button.ClickListener() {
 			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				// TODO Auto-generated method stub
@@ -96,4 +155,11 @@ public class Medication extends VerticalLayout implements View {
 		return verticalLayout;
 		
 	}
+	
+	
+	public void enter(ViewChangeEvent event) {
+		calendarList.removeAllItems();
+		calendarList.addItems("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+	}
+	
 }
