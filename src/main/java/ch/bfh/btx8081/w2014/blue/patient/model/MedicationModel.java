ffe545138a44d2f393747459ce8f57c84e77d510
ghodm1 <ghodm1@bfh.ch>
2015-01-11 18:35:59 +0100

    Day and time added.

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/model/MedicationModel.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/model/MedicationModel.java
@@ -10,7 +10,25 @@ public class MedicationModel {
 	private String name;
 	private int dosage;
 	private String description;
+	private String day;
+	private String time;
 	
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
 	public MedicationModel() {
 		
 	}
