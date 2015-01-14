a26b1e32a84a41fa1e6c112cd94dbba48b706133
ghodm1 <ghodm1@bfh.ch>
2015-01-14 15:09:33 +0100

    completed ! connection with database done !

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Medication.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/gui/Medication.java
@@ -3,9 +3,12 @@
  */
 package ch.bfh.btx8081.w2014.blue.patient.gui;
 
+import ch.bfh.btx8081.w2014.blue.patient.controller.ControllerMedication;
 import ch.bfh.btx8081.w2014.blue.patient.controller.ControllerUI;
+import ch.bfh.btx8081.w2014.blue.patient.model.MedicationModel;
 
-import com.vaadin.data.Item;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
@@ -13,10 +16,7 @@ import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 
@@ -28,23 +28,31 @@ import com.vaadin.ui.Button.ClickEvent;
 public class Medication extends VerticalLayout implements View {
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8877988983449185624L;
 	final VerticalLayout layout;
 	private AbsoluteLayout mainLayout;
 	private VerticalLayout verticalLayout;
 	private ComboBox calendarList;
-	private Button showInfoButton;
+	//private Button showInfoButton;
 	private Button homeButton;
 	private PatientMainDesign design;
-	private Table medication;
+	private Table medicationsTable;
+	private ControllerMedication controllerMedication;
+	 
 
 	
 	/**
 	 * Constructs a MEDICATIONVIEW on the base of different parameters.
 	 */
 	
-	public Medication()
+	public Medication(ControllerMedication controllerMedication)
 	{
 		//buildVerticalLayout_1();
+		this.controllerMedication=controllerMedication;
+		//this.controllerMedication.setMedication(this);
 		design = new PatientMainDesign(this);
 		layout = design.getLayout();
 		mainLayout = design.getMainLayout();
@@ -78,52 +86,29 @@ public class Medication extends VerticalLayout implements View {
 		verticalLayout.setWidth("260px");
 		verticalLayout.setHeight("360px");
 		verticalLayout.setMargin(false);
-		
-		
-		/**
-		 * ComboBox for a list of days
-		 */
 
-		calendarList = new ComboBox();
-		calendarList.setInputPrompt("Select a Day");
-		calendarList.setHeight("-1px");
-		calendarList.setNewItemsAllowed(true);
-		calendarList.setImmediate(true);
-		calendarList.setNullSelectionAllowed(false);
+		createComboBox();
+		
 		verticalLayout.addComponent(calendarList);
 		verticalLayout.setComponentAlignment(calendarList, Alignment.TOP_CENTER);
 		
 		
-		/**
-		 * Button to confirm selected day
-		 */
-		
-		showInfoButton = new Button("Show Medication");
-		showInfoButton.setIcon(FontAwesome.INFO_CIRCLE);
-		showInfoButton.setImmediate(true);
-		showInfoButton.setWidth("80%");
-		showInfoButton.setHeight("-1px");
-		verticalLayout.addComponent(showInfoButton);
-		verticalLayout.setComponentAlignment(showInfoButton, Alignment.TOP_CENTER);
 		
 		
-		/**
-		 * * Table for a list of medication for selected day
-		 */
 		
-		medication=new Table ();
-		medication.setWidth("100%");
-		medication.setHeight("100%");
-		medication.addContainerProperty("Medication", String.class, null);
-		medication.addContainerProperty("Dosage", String.class,  null);
-		medication.addContainerProperty("Time", String.class, null);
-		Object cipralex = medication.addItem();
-		Item row1 = medication.getItem(cipralex);
-		row1.getItemProperty("Medication").setValue("Cipralex");
-		verticalLayout.addComponent(medication);
-		verticalLayout.setComponentAlignment(medication, Alignment.TOP_CENTER);
-		//mainLayout.addComponent(medication);
-		//mainLayout.addComponent(verticalLayout, "top:60.0px;left:30.0px;");
+	
+		
+		
+		medicationsTable=new Table("Please select a day");
+		medicationsTable.setWidth("100%");
+		medicationsTable.setHeight("100%");
+		medicationsTable.addContainerProperty("Medication", String.class, null);
+		medicationsTable.addContainerProperty("Dosage", Integer.class,  null);
+		medicationsTable.addContainerProperty("Time", String.class, null);
+		
+		verticalLayout.addComponent(medicationsTable);
+		verticalLayout.setComponentAlignment(medicationsTable, Alignment.TOP_CENTER);
+		
 		
 		
 		/**
@@ -156,10 +141,73 @@ public class Medication extends VerticalLayout implements View {
 		
 	}
 	
+	/**
+	 * ComboBox for a list of days
+	 */
+	private void createComboBox(){
+		
+		calendarList = new ComboBox();
+		calendarList.setInputPrompt("Select a Day");
+		calendarList.setHeight("-1px");
+		calendarList.setNewItemsAllowed(true);
+		calendarList.setImmediate(true);
+		calendarList.setNullSelectionAllowed(false);
+		
+		for(String day : controllerMedication.getMedicationDays())
+		{	
+			calendarList.addItem(day);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param day
+	 */
+	private void createMedicationTable(String day){
+		
+		//medicationsTable=new Table ();
+		
+		
+		medicationsTable.removeAllItems();
+		
+		for(MedicationModel medication: controllerMedication.getMedicationsList())
+		{
+			
+			if(day.equals(medication.getDay()))
+			{
+				
+				medicationsTable.addItem(new Object[] {medication.getName(),medication.getDosage(),medication.getTime()},null);
+	
+			}
+			
+		}
+		medicationsTable.setPageLength(medicationsTable.size());
+	}
+	
 	
 	public void enter(ViewChangeEvent event) {
+	
+		/*
+		for (String medicationName : controllerMedication.getMedicationDays()) {
+			calendarList.addItem(medicationName);
+			
+		}
+		*/
+		calendarList.addValueChangeListener(new ValueChangeListener() {
+			
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				// TODO Auto-generated method stub
+				String day=calendarList.getValue().toString();
+				createMedicationTable(day);
+				
+			}
+		});
+		
+		/*
 		calendarList.removeAllItems();
 		calendarList.addItems("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+		*/
 	}
 	
 }
