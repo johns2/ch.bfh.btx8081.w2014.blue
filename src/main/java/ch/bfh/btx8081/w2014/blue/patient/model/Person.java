a349ea5ea266dc6247e72b811b9b9f8a7612bada
ghodm1 <ghodm1@bfh.ch>
2015-01-14 17:09:43 +0100

    Wrote comments.

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/model/Person.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/model/Person.java
@@ -9,27 +9,48 @@ package ch.bfh.btx8081.w2014.blue.patient.model;
 
 public abstract class Person {
 	private int userID;
-	private String PersonName;
+	private String personName;
 	
+	/**
+	 * Construct a Person with given userID and given Name.
+	 * @param userID
+	 * @param Name
+	 */
 	public Person (int userID, String Name){
 		this.userID = userID;
-		this.PersonName = Name;
+		this.personName = Name;
 	}
 
+	/**
+	 *  
+	 * @return the UserID
+	 */
 	public int getUserID() {
 		return userID;
 	}
 
+	/**
+	 * 
+	 * @param userID set UserID with given userID
+	 */
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
 
+	/**
+	 * 
+	 * @return the persons name
+	 */
 	public String getPersonName() {
-		return this.PersonName;
+		return this.personName;
 	}
 
+	/**
+	 * set the persons name 
+	 * @param PersonName
+	 */
 	public void setPersonName(String PersonName) {
-		this.PersonName = PersonName;
+		this.personName = PersonName;
 	}
 
 }
