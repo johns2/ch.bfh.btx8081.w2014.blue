ac8ade8a86b5e8d8322ebbf765b3be19f30471e9
ghodm1 <ghodm1@bfh.ch>
2015-01-15 11:02:18 +0100

    modified !

--- src/main/java/ch/bfh/btx8081/w2014/blue/patient/controller/ControllerMedication.java
+++ src/main/java/ch/bfh/btx8081/w2014/blue/patient/controller/ControllerMedication.java
@@ -14,41 +14,37 @@ import ch.bfh.btx8081.w2014.blue.patient.model.MedicationModel;
  */
 public class ControllerMedication {
 
-	private List<MedicationModel> medications;
-	private Medication medication;
-	private ArrayList<String> medicationNames = new ArrayList<String>();
+	private static List<MedicationModel> medications;
+	private static ArrayList<String> medicationNames = new ArrayList<String>();
 	private ArrayList<Integer> medicationDosages = new ArrayList<Integer>();
 	private ArrayList<String> medicationDescriptions = new ArrayList<String>();
 	private ArrayList<String> medicationDays = new ArrayList<String>();
 	private ArrayList<String> medicationTime = new ArrayList<String>();
 
+	/**
+	 * construct a ControllerMedication with a List of MedicalModel
+	 * @param list
+	 */
 	public ControllerMedication(List<MedicationModel> list) {
 		this.medications = list;
 		//this.medication = medication;
 	}
 
-	/**
-	 * set the medication
-	 * @param medication
-	 */
-	public void setMedication(Medication medication) {
-		this.medication = medication;
-	}
-
+	
+	
 	
 	/**
 	 * Returns the names of the medication
 	 * 
 	 * @return medicationNames in a <code>ArrayList</code><String>
 	 */
-	public ArrayList<String> getMedicationNames() {
+	public static ArrayList<String> getMedicationNames() {
 		for (MedicationModel medication : medications) {
 			medicationNames.add(medication.getName());
 		}
 		return medicationNames;
 	}
 	
-	//**
 	
 	
 	/**
